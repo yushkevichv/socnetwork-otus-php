1ddd54fb2cd8a50c616b240859b7d0ce42715a9c
Vitaliy Yushkevich <yushkevichv@gmail.com>
2020-03-09 01:57:53 +0300

    move to tailwind users page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,9 @@
                             @auth
                                 <div class="hidden md:block">
                                     <div class="ml-10 flex items-baseline">
-                                        <a href="{{route('user.index')}}" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Users</a>
-                                        <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Chat</a>
+                                        <a href="{{route('home')}}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Home</a>
+                                        <a href="{{route('user.index')}}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Users</a>
+                                        <a href="{{route('chat.index')}}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Chats</a>
                                     </div>
                                 </div>
                             @endauth
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,60 +1,121 @@
 @extends('layouts.inner')
 
 @section('content')
-<div class="container">
-    <div class="row ">
-        <div class="col-md-8 offset-2">
-            <form method="get" class="form-inline" action="{{ route('user.index') }}" >
-                <div class="form-group mb-2 mx-sm-3">
-                    <input type="text" name="q" class="form-control" value="{{ $query }}" placeholder="Поиск">
-                </div>
 
-                <input type="submit" class="btn btn-primary mb-2" value="Искать">
-            </form>
+<div class="flex flex-col mx-auto my-5">
+    <form class="mt-6" action="{{ route('user.index') }}" method="GET">
+        <div class="sm:flex">
+            <input type="text" placeholder="Поиск" name="q"   value="{{ $query }}" class="block sm:max-w-xl w-full px-4 py-3 text-base leading-6 appearance-none border border-gray-300 shadow-none bg-white rounded-md placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+            <button class="mt-4 relative sm:mt-0 sm:h-auto sm:ml-4 block w-full sm:w-auto border border-transparent px-6 py-3 text-base leading-6 font-semibold leading-snug bg-gray-900 text-white rounded-md shadow-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800 transition ease-in-out duration-150 hover:bg-gray-600" :class="{ 'opacity-50 pointer-events-none': submitting, 'hover:bg-gray-600': !submitting }" :disabled="submitting">
+                <span :class="{ 'opacity-0': submitting }">Искать</span>
+                <span style="" x-show="true" class="absolute inset-0 flex items-center justify-center pointer-events-none opacity-0" :class="{ 'opacity-0': !submitting }">
+              <svg class="h-8 w-8 spin" viewBox="0 0 24 24">
+                <path class="text-gray-600" fill="currentColor" d="M12 21a9 9 0 100-18 9 9 0 000 18zm0-2a7 7 0 110-14 7 7 0 010 14z"></path>
+                <path class="text-gray-400" fill="currentColor" d="M12 3a9 9 0 010 18v-2a7 7 0 000-14V3z"></path>
+              </svg>
+            </span>
+            </button>
         </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Список пользователей</div>
+    </form>
+</div>
 
-                <div class="card-body">
-{{--                    <p>Всего пользователей: {{ $usersCount }} </p>--}}
+<div class="flex flex-col">
+    <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+            @if(count($users) > 0)
+                <table class="min-w-full">
+                    <thead>
+                    <tr>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Имя
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Фамилия
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Пол
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Город
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Возраст
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                    @foreach($users as $user)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="ml-4">
+                                        <div class="text-sm leading-5 font-medium text-gray-900">{{ $user->name }}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">{{ $user->last_name }}</div>
+                            </td>
 
-                    @if(count($users) > 0)
-                        <table class="table">
-                            <tr>
-                                <th>Имя</th>
-                                <th>Фамилия</th>
-                                <th>Пол</th>
-                                <th>Город</th>
-                                <th>Возраст</th>
-                                <th></th>
-                            </tr>
-                        @foreach($users as $user)
-                            <tr>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->last_name }}</td>
-                                <td>{{ $user->gender }}</td>
-                                <td>{{ $user->city }}</td>
-                                <td>{{ $user->age }}</td>
-                                <td><a href="{{route('user.show', ['id' => $user->id])}}">Посмотреть</a> </td>
-                            </tr>
-                        @endforeach
-                        </table>
-                    @endif
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                {{ $user->gender }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                {{ $user->city }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                {{ $user->age }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                <a href="{{route('user.show', ['id' => $user->id])}}" class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Посмотреть</a>
+                            </td>
+                        </tr>
+                    @endforeach
 
-                    <p>
-                        Текущая страница: {{ $page }} <br>
+                    </tbody>
+                </table>
+
+                <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+                    <div class="flex-1 flex justify-between sm:hidden">
                         @if($page != 1)
-                             <a href="{{route('user.index')}}">В начало</a> |
+                            <a href="{{route('user.index')}}" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                                В начало
+                            </a>
                         @endif
                         @if($isMoreExist && (count($users) > 0))
-                             <a href="{{route('user.index', ['page' => $page+1])}}">Следующая страница</a>
+                            <a href="{{route('user.index', ['page' => $page+1])}}" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                                Следующая страница
+                            </a>
                         @endif
-                    </p>
+
+                    </div>
+                    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+                        <div>
+                            <p class="text-sm leading-5 text-gray-700">
+                                Текущая страница:
+                                <span class="font-medium">{{ $page }}</span>
+                            </p>
+                        </div>
+                        <div>
+                          <span class="relative z-0 inline-flex shadow-sm">
+                            <a  href="{{route('user.index')}}" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150">
+                              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
+                              </svg>
+                            </a>
+
+                            <a type="button" href="{{route('user.index', ['page' => $page+1])}}" class="-ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150">
+                              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+                              </svg>
+                            </a>
+                          </span>
+                        </div>
+                    </div>
                 </div>
-            </div>
+                
+            @endif
         </div>
     </div>
 </div>
